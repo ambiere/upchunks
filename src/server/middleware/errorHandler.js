b10a02bb98ca973123ff8e5a010d8c6fd22a42c7
zhid0399123 <zhid0399123@gmail.com>
2023-12-30 15:00:42 +0300

    feat(errorHandler): custom error handler middleware

---
+++ src/server/middleware/errorHandler.js
@@ -0,0 +1,30 @@
+module.exports = function errorHandler(error, req, res, next) {
+  const isENOENT = error.code === "ENOENT";
+  const isENOFUP = error.code === "ENOFUP";
+  const isEMECFA = error.code === "EMEUCF";
+
+  isENOENT && ErrorUtil.handleENOENT(error, res);
+  isENOFUP && ErrorUtil.handleENOFUP(error, res);
+  isEMECFA && ErrorUtil.handleEMEUCF(error, res, req);
+  next(error);
+};
+
+const ErrorUtil = {
+  handleENOENT: (error, res) => {
+    res.status(404).json({ error: error.message, code: error.code, statusCode: 404 });
+  },
+
+  handleENOFUP: (error, res) => {
+    res.status(400).json({ error: error.message, code: error.code, statusCode: 400 });
+  },
+
+  handleEMEUCF: (error, res, req) => {
+    req.log.info(error);
+    res.status(500).json({
+      info: `Internal server error. Contact the support team. Id ${req.id}`,
+      error: error.message,
+      code: error.code,
+      statusCode: 500,
+    });
+  },
+};
