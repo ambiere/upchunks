cb2c8d16a86f64f1670e26789a2510ada2d165de
zhid0399123 <zhid0399123@gmail.com>
2023-12-30 21:11:46 +0300

    bump: version 0.0.0 → 0.1.0

--- src/server/routes/upchunks.js
+++ src/server/routes/upchunks.js
@@ -1,81 +1,81 @@
-const { Router } = require("express");
-const multer = require("multer");
-const path = require("path");
-const fs = require("fs");
-const Collection = require("../services/collection");
-const MongoDbClient = require("../services/mongodb");
-const handleFileChunks = require("../util/handleFileChunks");
+const { Router } = require("express")
+const multer = require("multer")
+const path = require("path")
+const fs = require("fs")
+const Collection = require("../services/collection")
+const MongoDbClient = require("../services/mongodb")
+const handleFileChunks = require("../util/handleFileChunks")
 
-const router = Router({ strict: true });
-const storage = multer.memoryStorage();
-const upload = multer({ storage });
+const router = Router({ strict: true })
+const storage = multer.memoryStorage()
+const upload = multer({ storage })
 
-router.post("/file", upload.single("file"), async function (req, res, next) {
+router.post("/file", upload.single("upfile"), async function (req, res, next) {
   try {
     if (!req.file) {
-      const error = new Error("No file uploaded :)");
-      error.code = "ENOFUP";
-      throw error;
+      const error = new Error("No file uploaded :)")
+      error.code = "ENOFUP"
+      throw error
     }
-    const buffer = req.file.buffer;
-    const filename = req.file.originalname;
-    const mimetype = req.file.mimetype;
-    const chunks = Number(req.body.chunks);
-    const chunk = Number(req.body.chunk);
-    const filesDir = path.join(__dirname, "../..", "files");
-    const filePath = filesDir + `/${filename}`;
+    const buffer = req.file.buffer
+    const filename = req.file.originalname
+    const mimetype = req.file.mimetype
+    const chunks = Number(req.body.chunks)
+    const chunk = Number(req.body.chunk)
+    const filesDir = path.join(__dirname, "../..", "files")
+    const filePath = filesDir + `/${filename}`
 
-    !fs.existsSync(filesDir) && fs.mkdirSync(filesDir);
+    !fs.existsSync(filesDir) && fs.mkdirSync(filesDir)
     if (chunks && chunk) {
-      const filename = req.body.originalname;
-      const filePath = filesDir + `/${filename}`;
-      const fileSize = Number(req.body.fileSize);
-      const { mergedFileSize, error } = await handleFileChunks({ filename, buffer, chunk, chunks, filesDir });
-      if (error) return next(error);
+      const filename = req.body.originalname
+      const filePath = filesDir + `/${filename}`
+      const fileSize = Number(req.body.fileSize)
+      const { mergedFileSize, error } = await handleFileChunks({ filename, buffer, chunk, chunks, filesDir })
+      if (error) return next(error)
       if (mergedFileSize === fileSize) {
         const filedata = {
           originalname: filename,
           mimetype: mimetype,
           size: fileSize,
           filePath: filePath,
-        };
-        const insertedId = await Collection.insertDocument("filedata", filedata);
-        return res.json({ fileId: insertedId, ...filedata });
+        }
+        const insertedId = await Collection.insertDocument("filedata", filedata)
+        return res.json({ fileId: insertedId, ...filedata })
       }
-      return res.json({ message: `chunk ${chunk}/${chunks} saved`, status: "OK" });
+      return res.json({ message: `chunk ${chunk}/${chunks} saved`, status: "OK" })
     }
-    await fs.promises.writeFile(filePath, buffer);
+    await fs.promises.writeFile(filePath, buffer)
     const filedata = {
-      originalname: filename,
-      mimetype: mimetype,
+      name: filename,
+      type: mimetype,
       size: req.file.size,
-      filePath: filePath,
-    };
-    const insertedId = await Collection.insertDocument("filedata", filedata);
-    res.json({ fileId: insertedId, ...filedata });
+      // filePath: filePath,
+    }
+    // const insertedId = await Collection.insertDocument("filedata", filedata);
+    res.json({ ...filedata })
   } catch (error) {
-    next(error);
+    next(error)
   }
-});
+})
 
 router.get("/file/download/:fileId", async function (req, res, next) {
   try {
-    const { _id, err } = MongoDbClient.generateObjectId(req.params.fileId);
-    if (err) return next(err);
-    const filedata = await Collection.getDocument("filedata", { _id });
+    const { _id, err } = MongoDbClient.generateObjectId(req.params.fileId)
+    if (err) return next(err)
+    const filedata = await Collection.getDocument("filedata", { _id })
     if (!filedata) {
-      const error = new Error("File not found :)");
-      error.code = "ENOENT";
-      throw error;
+      const error = new Error("File not found :)")
+      error.code = "ENOENT"
+      throw error
     }
-    const filePath = filedata.filePath;
-    await fs.promises.access(filePath);
-    res.setHeader("Content-Disposition", `attachment; filename=${path.basename(filePath)}`);
-    res.setHeader("Content-Type", filedata.mimetype);
-    fs.createReadStream(filePath).pipe(res);
+    const filePath = filedata.filePath
+    await fs.promises.access(filePath)
+    res.setHeader("Content-Disposition", `attachment; filename=${path.basename(filePath)}`)
+    res.setHeader("Content-Type", filedata.mimetype)
+    fs.createReadStream(filePath).pipe(res)
   } catch (error) {
-    next(error);
+    next(error)
   }
-});
+})
 
-module.exports = router;
+module.exports = router
