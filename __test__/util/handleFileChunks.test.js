855e3078e4f31c6fa571adc77355ece363b89eed
zhid0399123 <zhid0399123@gmail.com>
2023-12-31 13:34:28 +0300

    test(handleFileChunks.test): updated test suite

--- __test__/util/handleFileChunks.test.js
+++ __test__/util/handleFileChunks.test.js
@@ -1,14 +1,14 @@
-const t = require("tap");
-const path = require("path");
-const fs = require("fs");
-const handleFileChunks = require("../../src/server/util/handleFileChunks");
+const t = require("tap")
+const path = require("path")
+const fs = require("fs")
+const handleFileChunks = require("../../src/server/util/handleFileChunks")
 
-t.test("should merge chunks", async (t) => {
-  const filesDir = path.join(__dirname, "../..", "src/files");
-  const chunks = 3;
-  const chunkName = "chunk";
+t.test("should handle file chunks", async (t) => {
+  const filesDir = path.join(__dirname, "../..", "src/files")
+  const chunks = 3
+  const chunkName = "test.txt"
 
-  let iteration = 1;
+  let iteration = 1
   while (iteration <= 3) {
     const result = await handleFileChunks({
       filename: chunkName,
@@ -16,11 +16,11 @@ t.test("should merge chunks", async (t) => {
       chunk: iteration,
       chunks,
       filesDir,
-    });
+    })
 
     if (result.error === undefined) {
-      t.equal(result.mergedFileSize, 3);
+      t.equal(result.mergedFileSize, 3)
     }
-    iteration++;
+    iteration++
   }
-});
+})
