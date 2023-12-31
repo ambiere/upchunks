aeecb6f92d65cd2d5ddac22d9c472675aef16599
zhid0399123 <zhid0399123@gmail.com>
2023-12-31 13:33:05 +0300

    test(mergeParts.test): mkdir "/files" if not exists

--- __test__/util/mergeParts.test.js
+++ __test__/util/mergeParts.test.js
@@ -1,29 +1,30 @@
-const t = require("tap");
-const path = require("path");
-const fs = require("fs");
-const mergeParts = require("../../src/server/util/mergeParts");
+const t = require("tap")
+const path = require("path")
+const fs = require("fs")
+const mergeParts = require("../../src/server/util/mergeParts")
 
 t.test("should merge chunks", async (t) => {
-  const filesDir = path.join(__dirname, "../..", "src/files");
-  const chunksDir = path.join(__dirname, "..", "fixtures/chunks");
-  const chunks = 3;
-  const chunkName = "chunk";
+  const filesDir = path.join(__dirname, "../..", "src/files")
+  const chunksDir = path.join(__dirname, "..", "fixtures/chunks")
+  const chunks = 3
+  const chunkName = "chunk"
 
   t.before(async () => {
-    let iteration = 1;
-    !fs.existsSync(chunksDir) && fs.mkdirSync(chunksDir);
+    let iteration = 1
+    !fs.existsSync(chunksDir) && fs.mkdirSync(chunksDir)
+    !fs.existsSync(filesDir) && fs.mkdirSync(filesDir)
     while (iteration <= chunks) {
-      await fs.promises.writeFile(`${chunksDir}/${chunkName}.part_${iteration}`, String(iteration));
-      iteration++;
+      await fs.promises.writeFile(`${chunksDir}/${chunkName}.part_${iteration}`, String(iteration))
+      iteration++
     }
-  });
+  })
 
   const result = await mergeParts({
     filename: chunkName,
     filePath: `${filesDir}/test.txt`,
     chunksDir,
     chunks,
-  });
+  })
 
-  t.match(result, { status: "done" });
-});
+  t.match(result, { status: "done" })
+})
