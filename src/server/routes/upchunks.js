65cbe01bed46142e15950760ed29270b4218d362
zhid0399123 <zhid0399123@gmail.com>
2024-01-01 13:14:41 +0300

    debug

--- src/server/routes/upchunks.js
+++ src/server/routes/upchunks.js
@@ -32,6 +32,7 @@ router.post("/file", upload.single("file"), async function (req, res, next) {
       const fileSize = Number(req.body.fileSize)
       const { mergedFileSize, error } = await handleFileChunks({ filename, buffer, chunk, chunks, filesDir })
       if (error) return next(error)
+      req.log.error(mergedFileSize, "merged filesize")
       if (mergedFileSize === fileSize) {
         const filedata = {
           originalname: filename,
