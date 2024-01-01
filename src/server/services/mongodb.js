cee194cba139161da866569c1171ddd5a9ad0813
zhid0399123 <zhid0399123@gmail.com>
2024-01-01 11:58:23 +0300

    chore(mongodb): changed database name

--- src/server/services/mongodb.js
+++ src/server/services/mongodb.js
@@ -1,38 +1,38 @@
-require("dotenv").config();
-const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config()
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb")
 
 class MongoDbClient {
   serverApiOptions = {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  };
-  mongoClientOptions;
-  client;
+  }
+  mongoClientOptions
+  client
 
   constructor(isProduction) {
-    this.mongoClientOptions = isProduction ? { serverApi: this.serverApiOptions } : {};
-    this.client = new MongoClient(process.env.MONGO_URL, this.mongoClientOptions);
+    this.mongoClientOptions = isProduction ? { serverApi: this.serverApiOptions } : {}
+    this.client = new MongoClient(process.env.MONGO_URL, this.mongoClientOptions)
   }
 
-  async getDatabase(databaseNme = "filedata") {
-    await this.client.connect();
+  async getDatabase(databaseNme = "upchunks") {
+    await this.client.connect()
     return {
       db: this.client.db(databaseNme),
       client: this.client,
-    };
+    }
   }
 
   static generateObjectId(inputId) {
     try {
-      return { _id: new ObjectId(inputId) };
+      return { _id: new ObjectId(inputId) }
     } catch (error) {
-      const err = new Error(error);
-      err.statusCode = 500;
-      return { err };
+      const err = new Error(error)
+      err.statusCode = 500
+      return { err }
     }
   }
 }
 
-module.exports = MongoDbClient;
-module.exports.MongoDbclient = MongoDbClient;
+module.exports = MongoDbClient
+module.exports.MongoDbclient = MongoDbClient
