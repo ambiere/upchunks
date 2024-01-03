53ecd420fabad36972ee80260691c045252e6e55
zhid0399123 <zhid0399123@gmail.com>
2024-01-03 17:28:50 +0300

    debug

--- src/server/util/mergeParts.js
+++ src/server/util/mergeParts.js
@@ -1,23 +1,23 @@
-const fs = require("fs");
+const fs = require("fs")
 
 async function mergeParts({ filename, filePath, chunksDir, chunks }) {
-  const stream = fs.createWriteStream(filePath);
-  let iteration = 1;
+  const stream = fs.createWriteStream(filePath)
+  let iteration = 1
   try {
     while (iteration <= chunks) {
-      const chunkPath = `${chunksDir}/${filename}.part_${iteration}`;
-      const buffer = await fs.promises.readFile(chunkPath);
-      stream.write(buffer);
-      fs.unlinkSync(chunkPath);
-      iteration++;
+      const chunkPath = `${chunksDir}/${filename}.part_${iteration}`
+      const buffer = await fs.promises.readFile(chunkPath)
+      stream.write(buffer)
+      // fs.unlinkSync(chunkPath);
+      iteration++
     }
-    stream.end();
-    return { status: "done" };
+    stream.end()
+    return { status: "done" }
   } catch (error) {
-    console.error(error);
-    return { status: "error", error };
+    console.error(error)
+    return { status: "error", error }
   }
 }
 
-module.exports = mergeParts;
-module.exports.mergeParts = mergeParts;
+module.exports = mergeParts
+module.exports.mergeParts = mergeParts
